25f609fd698324e3c830e72533c472a75816e76e
D4V1D98XD <124410459+D4V1D98XD@users.noreply.github.com>
2023-06-15 15:42:40 -0700

    Update PRACTICA7.php

--- parcial3/PRACTICA7.php
+++ parcial3/PRACTICA7.php
@@ -2,38 +2,50 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Serpientes y escaleras</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Practica 7</title>
     <link rel="stylesheet" href="css/bootstrap.css">
     <style>
-        .col-1.card{
-            height: 70px;
-            width: 6.5%;
-            text-align: center;
-            border-radius: 3%;
-            margin: 1.9px;
+        .casilla{
+            height: 50px;
+            font-weight: bold;
+        }
+        .escalera{
+            background-image: url('Escalerilla.png');
+            background-size: cover;
+        }
+        .serpiente{
+            background-image: url('serpienta.png');
+            background-size: cover;
+            background-repeat: no-repeat;
         }
     </style>
 </head>
 <body>
     <?php
-        $colores = ["#EAFAF1","#82E0AA","#186A3B","#F5FAF7"];
+    $colores = ["#FF8FFA", "#8FCAFF", "#FFD38F", "#95E5E5", "#B395E5", "#FCC5FF ", "#9FF38A"];
+
     ?>
     <div class="container">
-        <h1>Practica 7</h1>
-        <form action="">
-            <div class="row">
+        <center><h1><i><b><br>Tablero de Serpientes y Escaleras</b></i></h1></center><br>
+            <form action="">
+                <div class="row">
                 <?php
-                    for($i=100; $i>0; $i--){
-                        echo "<div class='col-1 card' style='background-color:".$colores[rand(0,3)].";'>".$i."</div>";
-                        /*if($i == 4){
+                    for($i=110; $i>0; $i--){
+                        if($i == 65||$i == 4||$i == 28||$i == 95||$i == 33||$i == 72||$i == 100||$i == 45){
+                        echo "<div class='col-1 card m-1 casilla escalera' style='background-color:".$colores[rand(0,6)].";'>".$i."</div>";
 
-                        }else if(){
-
-                        }*/
+                        }else  if($i == 80||$i == 10||$i == 48||$i == 86||$i == 52||$i == 2||$i == 107||$i == 99){
+                        echo "<div class='col-1 card m-1 casilla serpiente' style='background-color:".$colores[rand(0,6)].";'>".$i."</div>";
+                        }
+                        else{
+                        echo "<div class='col-1 card m-1 casilla' style='background-color:".$colores[rand(0,6)].";'>".$i."</div>";
+                        }
                     }
                 ?>
-            </div>
-        </form>
+            </form>
+        </div>
     </div>
 </body>
 </html>
